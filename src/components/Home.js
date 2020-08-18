3ba2f43714732aeda5c660b9fd56c4241c3ab578
Rocio D.C <roocio@gmail.com>
2020-08-18 13:05:01 -0300

    Bootstrap added

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Header from './Header'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <h1 class="mt-5">The Big Picture</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Deserunt voluptates rerum eveniet sapiente repellat esse,
+                doloremque quod recusandae deleniti nostrum assumenda vel beatae
+                sed aut modi nesciunt porro quisquam voluptatem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+export default Home

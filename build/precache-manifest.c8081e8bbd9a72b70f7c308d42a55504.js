dcff9048737331a395a0aba871d96c1c95ac236f
Rocio D.C <roocio@gmail.com>
2020-08-18 16:55:47 -0300

    build added

---
+++ build/precache-manifest.c8081e8bbd9a72b70f7c308d42a55504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0597ace2b23ac5a89bab8e901010c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11488f73a6a85f29a9c4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02a2055e6928c595a984",
+    "url": "/static/js/2.71e49040.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.71e49040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11488f73a6a85f29a9c4",
+    "url": "/static/js/main.5db16433.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
